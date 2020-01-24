bd970bb88c9c67d0527c539a9813f36e0feda733
Glenn Robert Strandos <strandos.glenn@gmail.com>
2020-01-24 20:25:43 +0100

    Testing firebase

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     });
 }
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT);
 
 exports.app;
