e4a6a63255c836b15f9020baf64304b3c3b88acd
Glenn Robert Strandos <strandos.glenn@gmail.com>
2020-01-25 18:14:33 +0100

    Using Routes constant instead of hardcoded string for api call

--- src/modules/league-watcher/redux/actions/index.ts
+++ src/modules/league-watcher/redux/actions/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { routes } from 'src/common/globals/routes/routes';
 import { showAlert } from 'src/common/state/actions';
 import { AlertType, TOGGLE_PROGRESS_BAR } from 'src/common/state/actions/types';
 import { FETCH_MATCH_HISTORY, LOAD_MORE, SET_PAGINATION } from 'src/modules/league-watcher/redux/actions/types';
@@ -7,7 +8,7 @@ export const fetchMatchHistory = (summonerName: string, position: number) => asy
     dispatch({ type: TOGGLE_PROGRESS_BAR, payload: true });
 
     try {
-        const response = await axios.post('/api/league/summoner', { summonerName, position });
+        const response = await axios.post(routes.api.league, { summonerName, position });
 
         dispatch({ type: FETCH_MATCH_HISTORY, payload: response.data });
         dispatch({ type: SET_PAGINATION, payload: { summonerName, position } });
@@ -21,7 +22,7 @@ export const loadMore = (summonerName: string, position: number) => async (dispa
     dispatch({ type: TOGGLE_PROGRESS_BAR, payload: true });
 
     try {
-        const response = await axios.post('/api/league/summoner', { summonerName, position });
+        const response = await axios.post(routes.api.league, { summonerName, position });
 
         dispatch({ type: LOAD_MORE, payload: response.data });
         dispatch({ type: SET_PAGINATION, payload: { summonerName, position } });
